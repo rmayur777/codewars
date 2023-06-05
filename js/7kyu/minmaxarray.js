14937c7f19e4d79318c16083e6d48e2f458d6d26
Mayur Rathod <rmayur777@gmail.com>
2023-06-05 12:55:22 +0530

    7k

---
+++ js/7kyu/minmaxarray.js
@@ -0,0 +1,12 @@
+// Task
+
+// Write a function that returns both the minimum and maximum number of the given list/array.
+// Examples (Input --> Output)
+
+// [1,2,3,4,5] --> [1,5]
+// [2334454,5] --> [5,2334454]
+// [1]         --> [1,1]
+
+function minmax(arr){
+    return [Math.min(...arr), Math.max(...arr)]
+}
