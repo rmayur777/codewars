a12fcbc96370d941dfa67a8823fd710ef26bf258
Mayur Rathod <rmayur777@gmail.com>
2023-07-16 15:41:28 +0530

    split foreach

---
+++ js/6kyu/count-characters-in-string.js
@@ -0,0 +1,19 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}
+
+function count (string) {
+    let count = {}
+    string.split('').forEach(function(s){
+        count[s] ? count[s]++ : count[s] = 1;
+    })
+    return count
+}
+
+function count (string) {
+    return string.split('').reduce(function(counts,char) {
+        counts[char] = (counts[char] || 0) + 1
+        return counts
+    }, {})
+    
+}
