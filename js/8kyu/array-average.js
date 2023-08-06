ffd33322fe0cc322ee80edd7850922f5fa4272e8
Mayur Rathod <rmayur777@gmail.com>
2023-08-06 13:11:20 +0530

    reduce

---
+++ js/8kyu/array-average.js
@@ -0,0 +1,12 @@
+// Write a function which calculates the average of the numbers in a given list.
+
+// Note: Empty arrays should return 0.
+
+function findAverage(array) {
+    // your code here
+    if (array.length > 0){
+    return (array.reduce((sum,current) => sum + current, 0)/array.length)}
+    else{
+      return 0
+    }
+  }
