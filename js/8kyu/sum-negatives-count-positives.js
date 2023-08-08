6fcdb6eee62667264fe8cb25431b0ccbe04645f2
Mayur Rathod <rmayur777@gmail.com>
2023-08-08 13:23:10 +0530

    ternary

---
+++ js/8kyu/sum-negatives-count-positives.js
@@ -0,0 +1,26 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+
+// If the input is an empty array or is null, return an empty array.
+// Example
+
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+// function countPositivesSumNegatives(input) {
+//   if(input.length == 0) return[];
+//   let pos = input.filter(a => a> 0).length
+//   let neg = input.filter(b=> b<=0).reduce((a,b)=> a+b,0)
+//   return [pos,neg]
+// }
+
+function countPositivesSumNegatives(input) {
+    let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
