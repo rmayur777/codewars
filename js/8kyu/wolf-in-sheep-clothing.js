62e4a8e2de661006bb44f854508c28d6042a26f0
Mayur Rathod <rmayur777@gmail.com>
2023-08-29 13:28:58 +0530

    ternary

---
+++ js/8kyu/wolf-in-sheep-clothing.js
@@ -0,0 +1,15 @@
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+
+// Note: there will always be exactly one wolf in the array.
+// Examples
+
+// Input: ["sheep", "sheep", "sheep", "wolf", "sheep"]
+// Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
+
+// Input: ["sheep", "sheep", "wolf"]
+// Output: "Pls go away and stop eating my sheep"
+
+function warnTheSheep(queue) {
+    const position = queue.reverse().indexOf('wolf');
+    return position === 0 ? 'Pls go away and stop eating my sheep' : `Oi! Sheep number ${ position }! You are about to be eaten by a wolf!`;
+  }
