955872fb0027ab5b12e12bd9d5369c0d1899d5ff
Mayur Rathod <rmayur777@gmail.com>
2023-10-09 13:04:55 +0530

    replace

---
+++ c#/8kyu/remove-exclamation-marks.cs
@@ -0,0 +1,10 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+using System;
+public class Kata
+{
+  public static string RemoveExclamationMarks(string s)
+  {
+       return s.Replace("!", "");
+  }
+}
