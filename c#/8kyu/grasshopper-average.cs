8893607e9e53bbab487043280a9d8041b08d1fca
Mayur Rathod <rmayur777@gmail.com>
2023-10-20 12:11:25 +0530

    avergae

---
+++ c#/8kyu/grasshopper-average.cs
@@ -0,0 +1,13 @@
+// Find Mean
+
+// Find the mean (average) of a list of numbers in an array.
+
+using System.Linq;
+
+public class GrassHopper 
+    {
+        public static int FindAverage(int[] nums)
+        {
+            return (int)nums.Average();
+        }
+    }
