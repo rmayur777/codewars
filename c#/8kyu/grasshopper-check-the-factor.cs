f1bef471e229ce88834ec1daa12fef87c964f280
Mayur Rathod <rmayur777@gmail.com>
2023-10-23 17:50:48 +0530

    factor

---
+++ c#/8kyu/grasshopper-check-the-factor.cs
@@ -0,0 +1,9 @@
+// This function should test if the factor is a factor of base.
+
+// Return true if it is a factor or false if it is not.
+
+
+public class Kata
+{
+  public static bool CheckForFactor(int num, int factor) => num % factor == 0;
+}
