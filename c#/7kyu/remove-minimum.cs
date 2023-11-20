ad933e42c8fa8b6283486ee38995ca72e3adaff1
Mayur Rathod <rmayur777@gmail.com>
2023-11-20 22:59:04 +0530

    where

---
+++ c#/7kyu/remove-minimum.cs
@@ -0,0 +1,29 @@
+// Task
+
+// Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+// Don't change the order of the elements that are left.
+// Examples
+
+// * Input: [1,2,3,4,5], output = [2,3,4,5]
+// * Input: [5,3,2,1,4], output = [5,3,2,4]
+// * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Remover
+{
+  public static List<int> RemoveSmallest(List<int> numbers)
+  {
+    if (numbers.Any() == false) return new List<int>();
+    
+    var min = numbers.Min();
+    var targetIndex = numbers.IndexOf(min);
+      return 
+        targetIndex != -1
+          ? numbers.Where((n,i) => i != targetIndex).ToList()
+          : numbers;
+  }
+}
