4443b4b313acab1c8f9348400dd114215eb76e28
Mayur Rathod <rmayur777@gmail.com>
2023-12-03 14:01:15 +0530

    method

---
+++ c#/8kyu/grasshopper-combine-strings.cs
@@ -0,0 +1,10 @@
+// Combine strings function
+
+// Create a function named (Combine_names) that accepts two parameters (first and last name). The function should return the full name.
+
+class Kata{
+   public static string  CombineNames(string n, string m)
+  {
+    return n + " "+ m;
+  }
+}
