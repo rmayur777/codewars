c0b4fc0e9f6172b5b22f4b9a7486513f46549dc4
Mayur Rathod <rmayur777@gmail.com>
2023-12-10 13:11:39 +0530

    ez

---
+++ c#/8kyu/five-without-numbers.cs
@@ -0,0 +1,11 @@
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+class Kata
+{
+  public static int UnusualFive()
+  {
+    return "abcde".Length;
+  }
+}
