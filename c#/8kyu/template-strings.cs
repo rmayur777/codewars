64d4bf187be9105ad1ca2288b4c8e882580ecec8
Mayur Rathod <rmayur777@gmail.com>
2024-02-14 15:32:16 +0530

    temp

---
+++ c#/8kyu/template-strings.cs
@@ -0,0 +1,8 @@
+// Your task is to return the correct string using the Template String Feature. 
+
+public class Templates{
+  public static string TempleStrings(string obj, string feature)
+  {
+      return $"{obj} are {feature}";
+  }
+}
