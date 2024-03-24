67c038b08a443118eb14830ca3278f5446190ed9
Mayur Rathod <rmayur777@gmail.com>
2024-03-24 14:30:01 +0530

    oop

---
+++ c#/8kyu/color-ghost.cs
@@ -0,0 +1,27 @@
+// Color Ghost
+
+// Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+using System;
+
+public class Ghost
+{ private static string[] colors = {"white", "yellow", "purple", "red"};
+  private string color;
+
+  public Ghost()
+  {
+    color = colors[new Random().Next(colors.Length)];
+  }  
+
+    public string GetColor()
+    {
+        return color;
+    }
+}
