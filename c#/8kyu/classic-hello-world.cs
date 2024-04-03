4b197aefe7181c52653c72cf2f035749e311819c
Mayur Rathod <rmayur777@gmail.com>
2024-04-03 14:00:40 +0530

    oop

---
+++ c#/8kyu/classic-hello-world.cs
@@ -0,0 +1,10 @@
+// You are given a method called main, make it print the line Hello World!, (yes, that includes a new line character at the end) and don't return anything
+
+// Note that for some languages, the function main is the entry point of the program.
+
+using System;
+
+public static class Solution
+{
+  public static void Main(string[] args) => Console.WriteLine("Hello World!");
+}
